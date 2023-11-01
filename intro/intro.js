c55946967b1ced9edfd5911ecd91d00ca13e477b
rahul more <rahulmore@rahuls-MacBook-Air.local>
2023-11-01 07:45:26 +0530

    funcitons

---
+++ intro/intro.js
@@ -0,0 +1,8 @@
+console.log("follow me on medium");
+//string
+var title = "Welcome to typeScript";
+// Number -- interger & floats
+var tutorialNum = 1;
+// Boolean
+var userIsAdmin = true;
+console.log(title, tutorialNum, userIsAdmin);
--- intro/intro.ts
+++ intro/intro.ts
@@ -1,1 +1,20 @@
 console.log("follow me on medium");
+
+//string
+
+let title: string = "Welcome to typeScript";
+
+// Number -- interger & floats
+let tutorialNum: number = 1;
+
+// Boolean
+let userIsAdmin: boolean = true;
+
+console.log(title, tutorialNum, userIsAdmin);
+
+// Functions
+const loginUser = (name: string, email: string, isAdmin: boolean = false) => {};
+
+// u have to pass 3 values to the function ,if u miss one of them it will through error, to avoid this declare some values as default like isAdmin:false
+
+loginUser("rahul", "rahul@MediaList.com");
